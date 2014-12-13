f2a3b935a1ceaf62a20df8c001af2cec17c3a037
Carel de Waal <dewaal.carel@gmail.com>
2014-12-13 19:58:49 +0000

    Added pi camera stream to website

--- python-webserver/server.py
+++ python-webserver/server.py
@@ -1,7 +1,11 @@
-from bottle import route, run
+from bottle import route, run, static_file
 
-@route('/hello')
-def hello():
-    return "Hello World!"
+@route('/')
+def static_main():
+    return static_file('main.html' , root='./static/')
+
+@route('/cam01_script.js')
+def static_main():
+    return static_file('cam01_script.js' , root='./static/')
 
 run(host='0.0.0.0', port=9977, debug=True)
