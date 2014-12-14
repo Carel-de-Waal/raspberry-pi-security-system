950d13bf5817cd33ef8e1b78c22f4a326e67efe6
Carel de Waal <dewaal.carel@gmail.com>
2014-12-14 13:04:27 +0000

    Updated controller

--- python-gpio-controller/controller.py
+++ python-gpio-controller/controller.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time, sys
+from time import sleep
+from threading import Thread
 
 #Define input ports
 PORT_FOB01=11 #Wireless remote used to activate/deactivate alarm 
@@ -9,35 +11,119 @@ PORT_UNUSED=15
 PORT_UNUSED=16
 PORT_UNUSED=18
 
-#Define output port
+#Define output ports
 PORT_SIREN01=22
+
 
 #An interrupt is called when PIR01 is triggered
 def isrFOB01(channel):
-	print('FOB01')
+	#print('FOB01')
+	if isArmed():
+		disarm()
+	else:
+		arm()
 
 #An interrupt is called when PIR01 is triggered
 def isrPIR01(channel):
-	print('PIR01')
+	if isArmed():
+		initSiren()
+	else:
+		print "PIR01 triggered but alarm is disarmed"
 
-#An interrupt is called when PIR01 is triggered
+#An interrupt is called when PIR02 is triggered
 def isrPIR02(channel):
 	print('PIR02')
 
+#Check wheter alarm is currently armed
+def isArmed():
+	f = open('isAlarmActivated','r')
+	isArmed=f.read()
+	f.close()
+	if isArmed.find("1")==-1: 
+		return False;
+	else:
+		return True;
+#Arm the system 
+def arm():
+	print "Alarm Status - ***ARMED***"
+	f = open('isAlarmActivated','w')
+	f.write('1')
+	f.close()
+	sirenWarn()
+
+#Disarm the system
+def disarm():
+	print "Alarm Status - ***DISARMED***"	
+	f = open('isAlarmActivated','w')
+	f.write('0')
+	f.close()
+	sirenWarn()
+	sleep(0.3)	
+	sirenWarn()
+
+#Initiate Siren activation sequence
+def initSiren():
+	#Give a warning siren
+	sirenWarn()
+	print "Wait 5 seconds"
+	sleep(5)
+	#Check if still armed
+	if isArmed() is False:
+		print "Siren sequence stopped"
+		return
+	else:
+		print "Siren still armed"
+	sirenWarn()
+	sleep(10)
+	#Check if still armed
+	if isArmed() is False:
+		print "Siren sequence stopped"
+		return
+	else:
+		print "Siren still armed"
+	sirenActivate()
+
+#Give a warning beep on Siren
+def sirenWarn():
+	print "Siren warning"
+	GPIO.output(PORT_SIREN01,1)
+	sleep(0.3)
+	GPIO.output(PORT_SIREN01,0)
+
+#Fully Activate the alarm
+def sirenActivate():
+	print "Siren is  Activated"
+	GPIO.output(PORT_SIREN01,1)
+	count=0
+	while True:
+		if isArmed():
+			print "Still Activated"
+		else:
+			print "Siren sequence stopped"
+			GPIO.output(PORT_SIREN01,0)
+			break
+		if count > 300 :
+			print "Siren sequence stopped - 3 minutes passed"
+			GPIO.output(PORT_SIREN01,0)
+			break
+		count=count+1
+		print count
+		sleep(1)
+			
+
+#=============================MAIN==================================
 #We will use the physical port number
 GPIO.setmode(GPIO.BOARD)  
 
-#
 GPIO.setup(PORT_FOB01 , GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PORT_PIR01 , GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(PORT_PIR02 , GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PORT_SIREN01 , GPIO.OUT)
 
-#Setup callback when pin 
-GPIO.add_event_detect(PORT_FOB01 , GPIO.RISING, callback=isrFOB01, bouncetime=3000)
-GPIO.add_event_detect(PORT_PIR01 , GPIO.RISING, callback=isrPIR01, bouncetime=3000)
-GPIO.add_event_detect(PORT_PIR02 , GPIO.RISING, callback=isrPIR02, bouncetime=3000)
 
+#Setup callback when pin 
+GPIO.add_event_detect(PORT_FOB01,GPIO.RISING, callback=isrFOB01, bouncetime=3000)
+GPIO.add_event_detect(PORT_PIR01,GPIO.RISING, callback=isrPIR01, bouncetime=3000)
+#GPIO.add_event_detect(PORT_PIR02 , GPIO.RISING, callback=isrPIR02, bouncetime=3000)
 
 
 while True:
@@ -45,6 +131,8 @@ while True:
         print "I am Alive"
         time.sleep(300)
     except KeyboardInterrupt:
-        print "Bye"
+        print "Cleanup and exit"
+	GPIO.output(PORT_SIREN01,0)
+	GPIO.cleanup()
         sys.exit()
 
